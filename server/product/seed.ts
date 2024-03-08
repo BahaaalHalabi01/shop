998eaf6658be674a220a43f9c8531f6e28eccb68
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-03-09 02:56:44 +0300

    seed sales

--- server/product/seed.ts
+++ server/product/seed.ts
@@ -1,4 +1,5 @@
-import { products } from "@/lib/db_schema";
+import { TCreateSale, products, sales, salesToProducts } from "@/lib/db_schema";
+import { faker } from "@faker-js/faker";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import { createTovar } from "./mock";
@@ -12,9 +13,35 @@ function seed() {
   const db = drizzle(turso);
 
   async function seedProducts() {
-    const items = new Array(12).fill(0).map(createTovar);
+    await db.transaction(async (tsx) => {
+      const items = new Array(28).fill(0).map(createTovar);
 
-    let res = await db.insert(products).values(items).returning();
+      await tsx.insert(products).values(items).returning();
+
+      let sale_products: TCreateSale[] = [];
+
+      new Array(50).fill(0).map((_) =>
+        sale_products.push({
+          day: faker.date.between({
+            from: new Date(2024, 3, 4),
+            to: new Date(2024, 3, 12),
+          }),
+          amount: faker.number.int({ max: 50, min: 5 }),
+          customer: faker.person.fullName(),
+        }),
+      );
+
+      const sales_products = (
+        await tsx.insert(sales).values(sale_products).returning()
+      ).map((sale) => ({
+        saleId: sale.id,
+        saleDay: sale.day,
+        productId: faker.number.int({ min: 0, max: items.length }),
+      }));
+
+      await tsx.insert(salesToProducts).values(sales_products);
+    });
+
     // await search_client.index(SearchIndex.products).addDocuments(res);
   }
 
