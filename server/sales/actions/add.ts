1dbc355f921927945cc25eddf45398798cec7ba4
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-03-09 03:36:11 +0300

    remove stock from products

--- server/sales/actions/add.ts
+++ server/sales/actions/add.ts
@@ -1,9 +1,21 @@
 "use server";
 import { revalidatePath } from "next/cache";
 import db from "@/lib/db";
-import { TCreateSale, sales, salesToProducts } from "@/lib/db_schema";
+import { eq } from "drizzle-orm";
+import { TCreateSale, products, sales, salesToProducts } from "@/lib/db_schema";
 
 export async function create_sale(values: TCreateSale & { productId: string }) {
+  const product = (
+    await db
+      .select()
+      .from(products)
+      .where(eq(products.id, Number(values.productId)))
+  )[0];
+
+  if (!product) {
+    return false;
+  }
+
   await db.transaction(async (tsx) => {
     const [item] = await tsx
       .insert(sales)
@@ -12,12 +24,17 @@ export async function create_sale(values: TCreateSale & { productId: string }) {
         amount: values.amount,
         day: values.day,
       })
-      .returning({ id: sales.id, day: sales.day });
+      .returning({ id: sales.id, day: sales.day, amount: sales.amount });
 
     await tsx.insert(salesToProducts).values({
       saleId: item.id,
       productId: Number(values.productId),
       saleDay: item.day,
+    });
+
+    const new_stock = product.stock - item.amount;
+    await tsx.update(products).set({
+      stock: new_stock > 0 ? new_stock : 0,
     });
   });
 
